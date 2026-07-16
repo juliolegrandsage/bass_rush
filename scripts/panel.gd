74e91b9b3e4984f678441687c37738581dfd49ad
juliolegrandsage <juliolgsage@gmail.com>
2026-07-16 18:22:31 +0200

    panel update

--- scripts/panel.gd
+++ scripts/panel.gd
@@ -3,7 +3,7 @@ extends Area2D
 var canInteract = false
 var hasEnteredPanelZone = false
 @onready var x_indicator = $"indicators"
-@onready var dialogue_box = $"../DialogueManager"
+@onready var dialogue_box = $"../CanvasLayer/DialogueManager"
 @onready var player = $"../CharacterBody2D"
 
 @export var message = ""
@@ -11,31 +11,24 @@ var hasEnteredPanelZone = false
 func _ready() -> void:
 	x_indicator.visible = false
 func _process(delta: float) -> void:
-	player.is_interacting = canInteract
-	if hasEnteredPanelZone:
-		if canInteract and Input.is_action_just_pressed("shoot"):
-			show_dialogue()
-		elif !canInteract and Input.is_action_just_pressed("shoot"):
-			quit_dialogue()
-			hasEnteredPanelZone = false
+	if Input.is_action_just_pressed("shoot"):
+		if canInteract and not dialogue_box.visible:
+			show_dialogue_panel()			
+		elif dialogue_box.visible and canInteract and Engine.time_scale == 0:
+			dialogue_box.quit_dialogue()
+			Engine.time_scale = 1
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		canInteract = true
 		hasEnteredPanelZone = true
 		x_indicator.visible = true
+		print(canInteract)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		canInteract = false
 		x_indicator.visible = false
-func show_dialogue():
-	dialogue_box.dialogue = message
+func show_dialogue_panel():
+	dialogue_box.new_text = ""
+	dialogue_box.new_text = message
 	dialogue_box.show_dialogue()
-	canInteract = false
-	print("message")
-	
-func quit_dialogue():
-	dialogue_box.quit_dialogue()
-	canInteract = true
-	
-	
