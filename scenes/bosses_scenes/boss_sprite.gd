ac0cec9c19b0b3417ab90366e4f2d565262af322
juliolegrandsage <juliolgsage@gmail.com>
2026-08-09 19:45:20 +0200

    added fade out

--- scenes/bosses_scenes/boss_sprite.gd
+++ scenes/bosses_scenes/boss_sprite.gd
@@ -1,8 +1,23 @@
 extends Sprite2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
-
+@onready var boss_controller = $"../demo_boss_controller"
+@onready var animation_player = $AnimationPlayer
+@onready var transition_to_menu = $"../CanvasLayer2"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready() -> void:
+	animation_player.play("RESET")
+
 func _process(delta: float) -> void:
-	position.x = player.position.x
+	if boss_controller.is_dead == false:
+		position.x = player.position.x
+	else:
+		launch_death_animation()
+		
+	
+func launch_death_animation():
+	animation_player.play("death")
+	await get_tree().create_timer(1.0).timeout
+	transition_to_menu.get_node("ColorRect/AnimationPlayer").play("fadeout")
